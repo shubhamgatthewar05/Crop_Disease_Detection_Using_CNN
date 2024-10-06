894a77fb878bec2410a3815fb5b4b1c4221439a3
Shubham Gatthewar <137514971+shubhamgatthewar05@users.noreply.github.com>
2024-10-06 09:52:44 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -2,19 +2,16 @@ import os
 import json
 from zipfile import ZipFile
 
-# Load Kaggle credentials from the local kaggle.json file
+
 with open("kaggle.json", "r") as file:
     kaggle_credentials = json.load(file)
 
-# Set Kaggle credentials as environment variables
 os.environ['KAGGLE_USERNAME'] = kaggle_credentials["username"]
 os.environ['KAGGLE_KEY'] = kaggle_credentials["key"]
 
-# Download the dataset from Kaggle
-# This command will run the Kaggle API to download the dataset
 os.system('kaggle datasets download -d abdallahalidev/plantvillage-dataset')
 
-# Unzip the downloaded dataset
+
 with ZipFile("plantvillage-dataset.zip", 'r') as zip_ref:
     zip_ref.extractall()
 
